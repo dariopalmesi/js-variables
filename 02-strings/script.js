49be3bf05bb7d99e9ab836d80d84ffd66b61f6ca
dariopalmesi <dariopalmesi88@gmail.com>
2024-09-27 16:44:21 +0200

    correct strings commit

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -14,12 +14,12 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = 'Jane Smith';
+const fullName = firstName +' '+ lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = 26;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
 
 let name = 'Dario';
